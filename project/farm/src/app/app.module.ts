21247efa1e961b8a8d62f170d44e2b37bf00c5b9
SevaniDileesha <32263957+SevaniDileesha@users.noreply.github.com>
2019-03-04 16:34:07 +0530

    make login and profile page

--- project/farm/src/app/app.module.ts
+++ project/farm/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import{FormsModule} from '@angular/forms';//registration form
 import{RouterModule} from '@angular/router';
-import{HttpClientModule}from '@angular/common/http';
+import{HttpClientModule,HTTP_INTERCEPTORS}from '@angular/common/http';
 //component
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -10,12 +10,21 @@ import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 //routes
 import{appRoutes} from './routes';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { UserService } from './shared/user.service';
+
+//other
+import { AuthGuard } from './auth/auth.guard';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    SignUpComponent
+    SignUpComponent,
+    UserProfileComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +32,11 @@ import{appRoutes} from './routes';
     RouterModule.forRoot(appRoutes),
     HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  },AuthGuard,UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- project/server/models/user.model.js
+++ project/server/models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 var userSchema = new mongoose.Schema({
     fullName: {
@@ -15,7 +15,7 @@ var userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: 'Password can\'t be empty',
-        minlength : [4,'Password must be atleast 4 character long']
+        minlength: [4, 'Password must be atleast 4 character long']
     },
     saltSecret: String
 });
@@ -26,7 +26,7 @@ userSchema.path('email').validate((val) => {
     return emailRegex.test(val);
 }, 'Invalid e-mail.');
 
-
+// Events
 userSchema.pre('save', function (next) {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(this.password, salt, (err, hash) => {
@@ -37,17 +37,20 @@ userSchema.pre('save', function (next) {
     });
 });
 
-//methods
-userSchema.methods.verifyPassword=function(password){
-    return bcrypt.compareSync(password,this.password);
+
+// Methods
+userSchema.methods.verifyPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
 };
 
-userSchema.methods.generateJwt=function(){
-    return jwt.sign({_id: this._id},
-       process.env.Jwt_SECRET,
-       {
-          expiresIn:process.env.Jwt_EXP
-       });
+userSchema.methods.generateJwt = function () {
+    return jwt.sign({ _id: this._id},
+        process.env.JWT_SECRET,
+    {
+        expiresIn: process.env.JWT_EXP
+    });
 }
 
-mongoose.model('User',userSchema);
+
+
+mongoose.model('User', userSchema);
