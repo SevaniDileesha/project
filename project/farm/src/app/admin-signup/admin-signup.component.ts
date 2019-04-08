75f225ef7b15a108e35bf3803849c00bc5ecf94b
SevaniDileesha <32263957+SevaniDileesha@users.noreply.github.com>
2019-04-08 23:41:07 +0530

    admin sign up

--- project/farm/src/app/admin-signup/admin-signup.component.ts
+++ project/farm/src/app/admin-signup/admin-signup.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+import { EmployeeService } from '../employee/employee.service';
+import { Employee } from '../employee/employee.model';
+
+declare var M: any;
 @Component({
   selector: 'app-admin-signup',
   templateUrl: './admin-signup.component.html',
-  styleUrls: ['./admin-signup.component.css']
+  styleUrls: ['./admin-signup.component.css'],
+  providers: [EmployeeService]
 })
 export class AdminSignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.resetForm();
+    this.refreshEmployeeList();
   }
+
+
+  resetForm(form?: NgForm) {
+    if (form)
+      form.reset();
+    this.employeeService.selectedEmployee = {
+      _id: "",
+      name: "",
+      position: "",
+      office: "",
+      salary: null
+    }
+  }
+
+
+  onSubmit(form: NgForm) {
+    if (form.value._id == "") {
+      this.employeeService.postEmployee(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshEmployeeList();
+        M.toast({ html: 'Saved successfully', classes: 'rounded' });
+      });
+    }
+    else {
+      this.employeeService.putEmployee(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.refreshEmployeeList();
+        M.toast({ html: 'Updated successfully', classes: 'rounded' });
+      });
+    }
+  }
+
+  refreshEmployeeList() {
+    this.employeeService.getEmployeeList().subscribe((res) => {
+      this.employeeService.employees = res as Employee[];
+    });
+  }
+
+  onEdit(emp: Employee) {
+    this.employeeService.selectedEmployee = emp;
+  }
+
+  onDelete(_id: string, form: NgForm) {
+    if (confirm('Are you sure to delete this record ?') == true) {
+      this.employeeService.deleteEmployee(_id).subscribe((res) => {
+        this.refreshEmployeeList();
+        this.resetForm(form);
+        M.toast({ html: 'Deleted successfully', classes: 'rounded' });
+      });
+    }
+  }
+
 
 }
